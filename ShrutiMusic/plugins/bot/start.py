efd6d3c7d917cb49557221c0e099cd07f9075f7b
Saksham gupta <163002102+Adibotkaiaia@users.noreply.github.com>
2026-02-01 10:53:22 +0530

    Update start.py

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -6,5 +6,13 @@ from ShrutiMusic import app
 @app.on_message(filters.command("start") & filters.private, group=1)
 async def force_start(client, message: Message):
     await message.reply_text(
-        "✅ /start WORKING NOW\n\nBot fully alive 🚀"
+        "✨ HELLO {}\n\n"
+        "🎶 WELCOME TO VIP X MUSIC 🎶\n\n"
+        "➤ A SMART & ELEGANT MUSIC BOT\n"
+        "➤ SMOOTH PLAYBACK • HD SOUND\n"
+        "➤ NO ADS • NO LAG\n\n"
+        "📌 SOURCES:\n"
+        "YouTube • Spotify • Apple • Saavn\n\n"
+        "👉 USE /help TO VIEW ALL COMMANDS",
+        format_kwargs={"mention": message.from_user.mention}
     )
